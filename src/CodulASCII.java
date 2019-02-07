d82f107f36050d15dc81a76ce3735bdbc462b41d
huguettevoicu <huguette.voicu@gmail.com>
2019-02-07 10:59:46 +0200

    IMPORT

---
+++ src/CodulASCII.java
@@ -0,0 +1,15 @@
+public class CodulASCII {
+    public static void main(String[] args) {
+        char i; int j;
+        j=0;
+        i = 0;
+        while (j<=127){
+            System.out.print(j);
+            System.out.print(' ');
+            System.out.println(i);
+            j++;
+            i++;
+
+        }
+    }
+}
